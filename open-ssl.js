644f35941cfe815ecb1149504a98473bf659af95
FirstTimeEZ <179362031+FirstTimeEZ@users.noreply.github.com>
2024-12-18 00:09:21 +1300

    simple-open-ssl

--- open-ssl.js
+++ open-ssl.js
@@ -4,16 +4,16 @@ import { platform } from 'os';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 
+const __plaformWindows = platform() === 'win32';
 const __opensslPath = join(dirname(fileURLToPath(import.meta.url)), "openssl", "bin", "openssl.exe");
 
 /**
- * Executes an `OpenSSL` command synchronously.
- * 
- * @remark
- * `Windows` - Will use the packaged version of `OpenSSL`, which should always work
- * 
- * @remark
- * `Linux` - Will use the local version of `OpenSSL`, which should be installed but can be installed with a package manager `... install openssl`
+ * Executes an OpenSSL command synchronously across different platforms.
+ *
+ * @description
+ * This function runs OpenSSL commands with platform-specific handling:
+ * - On Windows, it uses the packaged version of OpenSSL
+ * - On other platforms (e.g., Linux, macOS), it uses the locally installed OpenSSL
  *
  * @param {string} opensslCommand - The OpenSSL command to be executed, including any arguments.
  * @param {Object} [execOptions] - Optional execution options for the synchronous command, Defaults to UTF-8 encoding if not provided.
@@ -24,16 +24,7 @@ const __opensslPath = join(dirname(fileURLToPath(import.meta.url)), "openssl", "
  */
 export function runCommandSync(opensslCommand, execOptions = { encoding: 'utf-8' }) {
     try {
-        let pdc;
-
-        if (platform() === 'win32') {
-            pdc = `"${__opensslPath} " ${opensslCommand}`; // use packaged version
-        }
-        else {
-            pdc = `openssl ${opensslCommand}`; // use local version
-        }
-
-        return execSync(pdc, execOptions);
+        return execSync(__plaformWindows ? `"${__opensslPath} " ${opensslCommand}` : `openssl ${opensslCommand}`, execOptions);
     } catch {
         return undefined;
     }
